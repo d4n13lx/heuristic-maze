890f7f31fc9c3e11d45451211e3db81a310b7e44
d4n13lx <daniel.kazimoto83@gmail.com>
2026-05-19 21:44:52 -0300

    Atualizar o Labirinto.java

--- src/br/com/labirinto/model/Labirinto.java
+++ src/br/com/labirinto/model/Labirinto.java
@@ -13,13 +13,13 @@ public class Labirinto {
         this.linhas = linhas;
         this.colunas = colunas;
         this.mapa = new char[linhas][colunas];
-        this.posicaoRato = new Posicao(0, 0); // RF02: Rato sempre em [0,0]
+        this.posicaoRato = new Posicao(0, 0);
     }
 
     public void gerar() {
         Random rand = new Random();
 
-        // 1. Cria paredes externas, mas deixa a [0,0] como porta de entrada
+        // Cria paredes externas, mas deixa a [0,0] como porta de entrada
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
                 // Se for borda E NÃO for a posição [0,0], vira parede
@@ -31,14 +31,14 @@ public class Labirinto {
             }
         }
 
-        // 2. Coloca o Rato na entrada [0,0]
+        // Coloca o Rato na entrada [0,0]
         mapa[0][0] = 'R';
 
         // Garante que o rato consiga entrar no labirinto abrindo os espaços adjacentes
         if (colunas > 1) mapa[0][1] = ' ';
         if (linhas > 1) mapa[1][0] = ' ';
 
-        // 3. Coloca o Queijo em um lugar aleatório apenas no MIOLO do mapa
+        // Coloca o Queijo em um lugar aleatório apenas no MIOLO do mapa
         int qX, qY;
         do {
             // Sorteia entre 1 e tamanho-2 para o queijo não cair em cima das paredes externas
@@ -49,7 +49,7 @@ public class Labirinto {
         posicaoQueijo = new Posicao(qX, qY);
         mapa[qX][qY] = 'Q';
 
-        // 4. Espalha paredes internas (obstáculos) apenas no miolo do mapa
+        // Espalha paredes internas (obstáculos) apenas no miolo do mapa
         for (int i = 1; i < linhas - 1; i++) {
             for (int j = 1; j < colunas - 1; j++) {
                 if (i == qX && j == qY) continue; // Não apaga o queijo
